280f4cc76181760da5f0789e545809761c178213
elbasrie <ismailhasan1337@gmail.com>
2021-10-02 15:26:18 +0700

    Tugas 1

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Ismail Hasan Basri
+ Saya tingga di daerah Sidoarjo
+ olah raga yang saya sukai adalah
+ Renang";
+ }
+}

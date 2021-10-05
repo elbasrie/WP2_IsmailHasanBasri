194f95615d5df6c452d771a34102e09ba5f0265b
elbasrie <ismailhasan1337@gmail.com>
2021-10-05 12:54:46 +0700

    Tugas 3

--- application/controllers/Latihan1.php
+++ application/controllers/Latihan1.php
@@ -5,7 +5,7 @@ class Latihan1 extends CI_Controller
  {
  echo "Selamat Datang.. selamat belajar Web Programming";
  $this->load->view('view-latihan1');
- }
+ }[]
  public function penjumlahan($n1, $n2)
  {
     $this->load->model('Model_latihan1');
